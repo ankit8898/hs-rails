b214313fe91a58f116dc743ca8cf58172ec04670
Ankit Gupta <ankit.gupta8898@gmail.com>
2013-11-08 12:43:12 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HighStockTest::Application.config.secret_key_base = '63654e6b0b7b592a16750e844d695cd943c47665e1ae6f83e60b5026b433b40dd1f9b3be229f86a4a6a05149037d45e7e710795b9c323a262db14c606bcfd2f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HighStockTest::Application.config.session_store :cookie_store, key: '_high-stock-test_session'
